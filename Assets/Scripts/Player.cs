4ae055bb0bd46abf0203f9c5efa69d8841c41c13
Billel Guernouti <bguernouti@gmail.com>
2020-02-02 15:55:45 +0100

    First

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 2;
+
+    void Update() {
+        
+        if (Input.GetKey(KeyCode.UpArrow)){
+
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)){
+
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        
+    }
+}
